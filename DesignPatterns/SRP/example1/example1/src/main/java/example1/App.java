fd2be99c9a803dd1665f41298dcd8c3fc0206301
Richard Chao <richardchao2009@gmail.com>
2024-02-26 17:47:45 +0800

    Refactored userValidator

--- DesignPatterns/SRP/example1/example1/src/main/java/example1/App.java
+++ DesignPatterns/SRP/example1/example1/src/main/java/example1/App.java
@@ -12,12 +12,15 @@ public class App {
 	
 	public static void main(String[] args) throws IOException {
 		UserController controller = new UserController();
+		
 		//Check with valid JSON
 		String response = controller.createUser(VALID_USER_JSON);	
 		if(!response.equalsIgnoreCase("SUCCESS")) {
 			System.err.println("Failed");
 		}
 		System.out.println("Valid JSON received response: "+response);
+
+
 		//Check with invalid JSON
 		response = controller.createUser(INVALID_USER_JSON);
 		if(!response.equalsIgnoreCase("ERROR")) {
