7aa080151291d1ea91dbf69faa1fc1a86e8779a8
Richard Chao <richardchao2009@gmail.com>
2024-02-28 19:54:16 +0800

    Start files for open closed principle nad finished srp files

---
+++ DesignPatterns/Open-Closed Principle/example1/src/main/java/open_closed_principle/App.java
@@ -0,0 +1,13 @@
+package open_closed_principle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ DesignPatterns/SRP/example1/src/main/java/srp/UserPersistenceService.java
@@ -0,0 +1,11 @@
+package srp;
+
+//A separate class for handling persistence 
+public class UserPersistenceService {
+
+	private Store store = new Store();
+	
+	public void saveUser(User user) {
+		store.store(user);
+	}
+}
