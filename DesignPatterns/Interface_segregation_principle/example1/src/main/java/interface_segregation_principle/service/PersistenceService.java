69e4c20422b46928127afde2a9925d54e56992e0
Richard Chao <richardchao2009@gmail.com>
2024-03-05 18:56:09 +0800

    interface seg principle start file

---
+++ DesignPatterns/Interface_segregation_principle/example1/src/main/java/interface_segregation_principle/service/PersistenceService.java
@@ -0,0 +1,17 @@
+package interface_segregation_principle.service;
+
+import java.util.List;
+
+import interface_segregation_principle.entity.Entity;
+
+//common interface to be implemented by all persistence services. 
+public interface PersistenceService<T extends Entity> {
+
+	public void save(T entity);
+	
+	public void delete(T entity);
+	
+	public T findById(Long id);
+	
+	public List<T> findByName(String name);
+}
---
+++ DesignPatterns/Interface_segregation_principle/example1/src/test/java/interface_segregation_principle/AppTest.java
@@ -0,0 +1,20 @@
+package interface_segregation_principle;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
